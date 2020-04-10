43c2d9c7c5962309285693ba514c76ade61660b8
Todd Holmquist-Sutherland <toddhs@microsoft.com>
2020-04-11 00:01:54 +0200

    fixed missing numpy import

--- safedriver/training/train.py
+++ safedriver/training/train.py
@@ -26,6 +26,7 @@ POSSIBILITY OF SUCH DAMAGE.
 
 import os
 import pandas as pd
+import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 import lightgbm
