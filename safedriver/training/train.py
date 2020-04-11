8ea902ae73b40c964ed7d7ce758e569822308721
Todd Holmquist-Sutherland <toddhs@microsoft.com>
2020-04-11 07:41:58 +0200

    added import metrics from sklearn

--- safedriver/training/train.py
+++ safedriver/training/train.py
@@ -27,7 +27,7 @@ POSSIBILITY OF SUCH DAMAGE.
 import os
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error
+from sklearn import metrics
 from sklearn.model_selection import train_test_split
 import lightgbm
 
